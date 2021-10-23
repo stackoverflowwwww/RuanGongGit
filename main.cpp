0aef1270d8c279ed971ed7b73a4b98150becc954
bong <381209755@qq.com>
2021-10-23 21:18:00 +0800

    now has 2 files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    cout<<"hello git"<<endl;      
+    cout<<"hello git"<<endl; 
+    cout<<"hello ruan jian gong cheng"<<endl; 
     return 0;
 }
