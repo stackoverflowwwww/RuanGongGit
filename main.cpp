86a9e2d27f082ff5b2c3a6c0dad716a4549d033c
panjiajian <1059472636@qq.com>
2021-11-16 23:01:55 +0800

    must add print

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
+    // pjj_2019152052
     cout<<"pjj"<<endl;
     cout<<"hello git"<<endl; 
     cout<<"hello ruan jian gong cheng"<<endl; 
     return 0;
 }
+
